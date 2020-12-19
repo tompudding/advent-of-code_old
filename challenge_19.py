4e1659d1e55f038c7646474929674dcdf47908fd
Tom Gooding <tom.pudding@gmail.com>
2020-12-19 13:21:10 +0000

    Tidy up day 19

--- challenge_19.py
+++ challenge_19.py
@@ -26,19 +26,14 @@ class Text:
 
     def match(self, target):
         if target.startswith(self.text):
-            #print(f'Match text {self.text} for target "{target}"')
             yield target[len(self.text):]
-        else:
-            #print(f'No Match text {self.text} for target "{target}"')
-            #return False, target
-            pass
 
     def __repr__(self):
         return f'Text({self.text})'
 
 class Rule:
     # A rule consists of a list of parts, any of which can match entirely for it to be considered a match
-    def __init__(self, text, debug=False):
+    def __init__(self, text):
         self.rules = []
         self.is_text = False
 
@@ -55,20 +50,14 @@ class Rule:
 
         rule_list = []
         for n in [int(v) for v in text.split()]:
-            if debug:
-                print('XX',n,'|',len(rule_list),'|',rule_list)
 
             if n not in rules:
                 rules[n] = Rule(rules_text[n])
 
             rule_list.append(rules[n])
-            if debug:
-                print('YY',n,'|',len(rule_list),'|',rules[n],'|',rule_list[0])
-
 
         # any adjacent rules that are just strings can be appended
-        if debug:
-            print('NNN',rule_list)
+
         collapsed = []
         for pos in range(len(rule_list) - 1):
             if rule_list[pos].is_text and rule_list[pos+1].is_text:
@@ -76,24 +65,18 @@ class Rule:
             else:
                 collapsed.append(rule_list[pos])
 
-        if debug:
-            print('MMM',collapsed)
-
         collapsed.append(rule_list[-1])
 
         self.rules.append(collapsed)
 
     def match(self, target):
-        #print('jim',target)
-        #print(self)
-        #print(len(self.rules))
+
         for rule_list in self.rules:
             current = target
             match = False
 
             stack = (target,)
             for rule in rule_list:
-                #print(stack)
                 new_stack = []
                 for item in stack:
                     new_stack = itertools.chain(new_stack, (rest for rest in rule.match(item)))
@@ -101,18 +84,6 @@ class Rule:
 
             for item in stack:
                 yield item
-                # new_stack = []
-                # for item in stack:
-                #     for rest in rule.match(item):
-                #         new_stack.append(rest)
-                #         yield rest
-
-                # stack = new_stack
-
-            #if match:
-                #print(f'Match {self} for target "{target}"')
-                #return match, current
-        #return False, target
 
     def __repr__(self):
         out = []
@@ -129,7 +100,7 @@ def match_rule_zero():
     for line in data:
         for rest in rules[0].match(line):
             if not rest:
-                print('***',line)
+                #print('***',line)
                 count += 1
                 break
 
@@ -144,20 +115,11 @@ def expand(n, text):
 
     return ' | '.join(out)
 
-#for rest in Rule(rules_text[0]).match('ababbb'):
-#    print(rest)
-#raise SystemExit()
-
 print(f'Part 1 Total {match_rule_zero()}')
 
 rules = {}
 
 rules_text[11] = '42 31 | ' + expand('11', '42 11 31')
 rules_text[8] = '42 | ' + expand('8', '42 8')
-
-#rules_text[11] = '42 31 | 42 42 31 31'
-#rules_text[8] = '42 | 42 42'
-
-#for match, rest in Rule(rules_text[8]).match('babbbbaabbbbbabbbbbbaabaaabaaa')
 
 print(f'Part 2 Total {match_rule_zero()}')
